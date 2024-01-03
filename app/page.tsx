8eab8e5f5795ebacf030be4e98f874e0e793226c
Jarren San Jose <sanjose.jarren@gmail.com>
2024-01-02 22:47:23 -0800

    add image

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold">abasho</h1>
+      <h1 className="text-4xl font-bold mb-4">abasho</h1>
+      <Image src="/192.png" alt="abasho 192" width={500} height={500} />
     </main>
   )
 }
