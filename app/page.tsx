24eaaec6905b2b7864713ad48f766a168470b758
Jarren San Jose <sanjose.jarren@gmail.com>
2024-02-03 16:46:03 -0800

    change a tag to Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { FaTelegramPlane, FaDiscord, FaTwitter } from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -7,15 +8,15 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-4">abasho</h1>
       <Image src="/192.png" alt="abasho 192" width={500} height={500} />
       <div className="mt-4 flex space-x-4">
-        <a href="https://telegram.me/collablandbot?start=VFBDI1RFTCNDT01NIy0xMDAxNjQyNTAzNjY2" className="text-blue-500 hover:text-blue-700">
+        <Link href="https://telegram.me/collablandbot?start=VFBDI1RFTCNDT01NIy0xMDAxNjQyNTAzNjY2" className="text-blue-500 hover:text-blue-700" target="_blank">
           <FaTelegramPlane size={30} />
-        </a>
-        <a href="https://discord.com/invite/eP5EKaGssE" className="text-blue-500 hover:text-blue-700">
+        </Link>
+        <Link href="https://discord.com/invite/eP5EKaGssE" className="text-blue-500 hover:text-blue-700" target="_blank">
           <FaDiscord size={30} />
-        </a>
-        <a href="https://twitter.com/AbashoNFT" className="text-blue-500 hover:text-blue-700">
+        </Link>
+        <Link href="https://twitter.com/AbashoNFT" className="text-blue-500 hover:text-blue-700" target="_blank">
           <FaTwitter size={30} />
-        </a>
+        </Link>
       </div>
     </main>
   );
