0ca5f50d07b472827bf4bbbb5e34a97cbe52be21
Jason Takahashi <jmtakahashi@gmail.com>
2024-02-03 17:00:49 -0800

    change formatting.  single quotes implemented.  document formatting implemented.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'abasho',
   description: 'abasho',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
         {children}
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,26 @@ import { FaTelegramPlane, FaDiscord, FaTwitter } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold mb-4">abasho</h1>
-      <Image src="/192.png" alt="abasho 192" width={500} height={500} />
-      <div className="mt-4 flex space-x-4">
-        <a href="https://telegram.me/collablandbot?start=VFBDI1RFTCNDT01NIy0xMDAxNjQyNTAzNjY2" className="text-blue-500 hover:text-blue-700">
+    <main className='flex min-h-screen flex-col items-center p-24'>
+      <h1 className='text-4xl font-bold mb-4'>abasho</h1>
+      <Image src='/192.png' alt='abasho 192' width={500} height={500} />
+      <div className='mt-4 flex space-x-4'>
+        <a
+          href='https://telegram.me/collablandbot?start=VFBDI1RFTCNDT01NIy0xMDAxNjQyNTAzNjY2'
+          className='text-blue-500 hover:text-blue-700'
+        >
           <FaTelegramPlane size={30} />
         </a>
-        <a href="https://discord.com/invite/eP5EKaGssE" className="text-blue-500 hover:text-blue-700">
+        <a
+          href='https://discord.com/invite/eP5EKaGssE'
+          className='text-blue-500 hover:text-blue-700'
+        >
           <FaDiscord size={30} />
         </a>
-        <a href="https://twitter.com/AbashoNFT" className="text-blue-500 hover:text-blue-700">
+        <a
+          href='https://twitter.com/AbashoNFT'
+          className='text-blue-500 hover:text-blue-700'
+        >
           <FaTwitter size={30} />
         </a>
       </div>
