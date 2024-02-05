d10ef29d9b75274a1bff2ffcb2958252a569707a
Jason Takahashi <jmtakahashi@gmail.com>
2024-02-04 18:45:26 -0800

    add header component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
+import Header from '@/components/Header';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <Header />
         {children}
         <Analytics />
       </body>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 export default function Header() {
-  <header className='w-full p-2 flex justify-between items-center bg-neutral-400'>
-    <h1 className='text-4xl font-bold'>abasho</h1>;
-  </header>;
+  return (
+    <header className='w-full p-2 flex justify-between items-center bg-neutral-400'>
+      <h1 className='text-4xl font-bold'>abasho</h1>
+    </header>
+  );
 }
