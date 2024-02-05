fa7f1934d147c793f250fd91d356c98dba500d38
Jason Takahashi <jmtakahashi@gmail.com>
2024-02-04 18:42:52 -0800

    add components directory.

---
+++ components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  <header className='w-full p-2 flex justify-between items-center bg-neutral-400'>
+    <h1 className='text-4xl font-bold'>abasho</h1>;
+  </header>;
+}
