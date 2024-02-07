d637afd70a01baa584c0cdbd38985a60d0576d68
Jason Takahashi <jmtakahashi@gmail.com>
2024-02-07 00:56:38 -0800

    modifiy footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,30 +1,17 @@
-import { FaTelegramPlane, FaDiscord, FaTwitter } from 'react-icons/fa';
 import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className='mt-4 flex space-x-4'>
-      <Link
-        href='https://telegram.me/collablandbot?start=VFBDI1RFTCNDT01NIy0xMDAxNjQyNTAzNjY2'
-        className='text-blue-500 hover:text-blue-700'
-        target='_blank'
-      >
-        <FaTelegramPlane size={30} />
-      </Link>
-      <Link
-        href='https://discord.com/invite/eP5EKaGssE'
-        className='text-blue-500 hover:text-blue-700'
-        target='_blank'
-      >
-        <FaDiscord size={30} />
-      </Link>
-      <Link
-        href='https://twitter.com/AbashoNFT'
-        className='text-blue-500 hover:text-blue-700'
-        target='_blank'
-      >
-        <FaTwitter size={30} />
-      </Link>
+    <footer className='w-full'>
+      <div className='flex justify-between items-center p-4 py-8 sm:px-8 sm:py-10'>
+        <p className='text-xs text-neutral-50'>Abasho means hug</p>
+        <Link
+          href='https://etherscan.io/address/0xe9c79b33c3a06f5ae7369599f5a1e2ff886e17f0'
+          className='text-xs text-neutral-50 hover:underline'
+        >
+          Etherscan
+        </Link>
+      </div>
     </footer>
   );
 }
